880520d5306bb18b356c13ebf426b5bec102ba9c
Giorgio Perez <giorgiop62@gmail.com>
2023-01-31 18:44:31 +0100

    gestione errori

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -9,6 +9,28 @@ class LeadController extends Controller
 {
     function store($request){
         $data = $request->all();
+        $success = true;
+
+        $validator = validator::make($data,
+        [
+            'name' => 'required|min:3|max255',
+            'email' => 'required|min:3|max255|email',
+            'text' => 'required|min:5',
+        ]);
+
+        //controllo validazione
+        if($validator->fails()){
+            $success = false;
+            $errors = $validator->errors();
+            return response()->json(compact('success','errors'));
+        }
+
+        //salvo i dati nel db
+         $new_lead = new Lead();
+         $new_lead->fill($data);
+         $new_lead->save();
+
+         Mail::to('"hello@example.com"')->send(new  NewContact($new_lead));
 
 
         return response()->json($data);
--- resources/js/pages/Contacts.vue
+++ resources/js/pages/Contacts.vue
@@ -1,58 +1,71 @@
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-    name:'Contacts',
-    data(){
-        return{
-            name:'',
-            email:'',
-            text:''
+  name: "Contacts",
+  data() {
+    return {
+      name: "",
+      email: "",
+      text: "",
+    };
+  },
+  methods: {
+    sendForm() {
+      const data = {
+        name: this.name,
+        email: this.email,
+        text: this.text,
+      };
+      axios.post("http://127.0.0.1:8000/api/contacts", data).then((result) => {
+        if (!result.data.success) {
+          this.errors = result.data.errors;
+          console.log(this.errors);
         }
+      });
     },
-    methods:{
-        sendForm(){
-            const data =
-            {
-                name:  this.name,
-                email: this.email,
-                text:  this.text,
-            }
-            console.log(data);
-        }
-    }
-
-}
+  },
+};
 </script>
 
 <template>
-    <h1>contatti</h1>
-    <div class="container">
-        <h3>Numero:3505255178</h3>
-        <h3>Residenza:Napoli</h3>
-        <h3>Anno di nascits: 26/12/1995</h3>
-        <h3>Email:giorgiop62@gmail.com</h3>
+  <h1>contatti</h1>
+  <div class="container">
+    <h3>Numero:3505255178</h3>
+    <h3>Residenza:Napoli</h3>
+    <h3>Anno di nascits: 26/12/1995</h3>
+    <h3>Email:giorgiop62@gmail.com</h3>
 
-        <form @submit.prevent="sendForm()">
-            <div>
-                <input v-model.trim="name" type="text" placeholder="name">
-            </div>
-            <div>
-                <input v-model.trim="email" type="text" placeholder="email">
-            </div>
-            <div>
-                <textarea v-model.trim="text" name="text"  cols="30" rows="10" placeholder="text"></textarea>
-            </div>
+    <form @submit.prevent="sendForm()">
+      <div>
+        <input
+          v-model.trim="name"
+          type="text"
+          placeholder="name"
+        />
+      </div>
+      <div>
+        <input
+          v-model.trim="email"
+          type="text"
+          placeholder="email"
+        />
+      </div>
+      <div>
+        <textarea
+          v-model.trim="text"
+          name="text"
+          cols="30"
+          rows="10"
+          placeholder="text"
+        ></textarea>
+      </div>
 
-            <button type="submit" class="btn btn-primary">Invia</button>
-
-        </form>
-
-    </div>
-
+      <button type="submit" class="btn btn-primary">Invia</button>
+    </form>
+  </div>
 </template>
 
 
 
 <style>
-
 </style>
